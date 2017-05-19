7d827725e0747ef4f07dfeb2dbb614d9fc40b56e
Cristian <you@example.com>
2017-05-19 14:59:14 -0300

    Se agregaron mas libros

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
         Libro libro1 = new Libro("Harry Potter", "El misterio del principe","Fantasia",2004);
         Libro libro = new Libro("Seductor Infalible", "Seductor", "guia", 170);
+        Libro libro2 = new Libro("Teorias de Objetos", "Java Program", "Educacional",400);
 
         libro1.abrirElLibro();
 
         libro1.datosDelLibro();
 
-        libro.datosDelLibro();
+        libro2.datosDelLibro();
 
         System.out.print("");
     }
